e6f9d784deb0615d30dc386799416f904a6ebf46
Monish reddy bs <monish.reddy.b.s@ibm.com>
2024-04-29 17:58:51 +0530

    more java learning

--- JAVA/InttoString.java
+++ JAVA/InttoString.java
@@ -8,6 +8,8 @@ public class InttoString {
 
         int n1 = 10;
         Integer l = n1; // auto boxing
+        String l1 = l.toString(); // convert integer to string
+        System.out.println(l1);
         int p = l;       // auto unboxing
         System.out.println(p+" "+n1+" "+l); // 10 10 10
     }
